a6ed7e90b31849eb4ebe4de69da6ef2a8d52a296
Manuelbailey101 <115514197+Manuelbailey101@users.noreply.github.com>
2023-10-16 10:10:38 -0700

    currenUser null problem

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,17 +1,26 @@
+import { User } from "@prisma/client";
+
 import Container from "../container";
 import Logo from "./Logo";
 import Search from "./Search"
 import UserMenu from "./UserMenu";
 
-const Navbar = () => {
+interface NavbarProps {
+  currentUser?: User | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({
+  currentUser
+}) => {
+  console.log({ currentUser });
     return (
-        <div className="fixed w-full bg-white z-10 shadow-sm">
-          <div 
+      <div className="fixed w-full bg-white z-10 shadow-sm">
+        <div 
           className="
           py-4
           border-b-[1px]
           "
-          >
+        >
             <Container>
              <div
                className="
